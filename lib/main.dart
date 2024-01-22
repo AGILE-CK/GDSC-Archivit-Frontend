dd946d825c3e6b5ce0a5bca8649f66aef341a43b
Chae Hyeong Lim <cogud1107@gmail.com>
2024-01-22 14:45:49 +0900

    refactor: navigator => Get

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gdsc/Login/login_page.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       fontFamily: 'Pretendard',
