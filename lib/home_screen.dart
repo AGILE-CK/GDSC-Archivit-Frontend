94bbfd818e0d637c4075e91ffd2cac40b2ed324d
Chae Hyeong Lim <cogud1107@gmail.com>
2024-01-23 23:50:49 +0900

    home screen
    홈스크린 및 글씨체

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,38 +6,43 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryColor,
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 200.0,
-            floating: false,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('Downloads'),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.only(left: 16.0, top: 44.0),
+              child: Text(
+                'Archivit',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'HGGGothicssi_Pro',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w900,
+                  height: 1.2,
+                  letterSpacing: 1.0,
+                ),
+              ),
             ),
           ),
-          SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-            ),
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return Container(
-                  color: index % 2 == 0 ? lightColor : darkColor,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.file_download, color: primaryColor, size: 48.0),
-                      SizedBox(height: 8.0),
-                      Text('File $index', style: TextStyle(color: primaryColor)),
-                    ],
+          SliverToBoxAdapter(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Container(
+                height: 36.0, // 검색 상자의 높이
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    filled: true,
+                    fillColor: Colors.grey[500], // 회색 배경색
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none, // 테두리 선 없음
+                      borderRadius: BorderRadius.circular(5.0), // 둥근 모서리
+                    ),
                   ),
-                );
-              },
-              childCount: 20,
+                ),
+              ),
             ),
           ),
         ],
